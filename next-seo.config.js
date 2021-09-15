902f963bb6916655960402c4701bea2585aa988b
Mikkel Petersen <mikkel@mikkelpetersen.dk>
2021-09-15 22:36:44 +0200

    Added SEO Config

---
+++ next-seo.config.js
@@ -0,0 +1,10 @@
+export const SEO = {
+  titleTemplate: "Mikkel Petersen - %s",
+  defaultTitle: "Mikkel Petersen",
+  openGraph: {
+    type: "website",
+    locale: "en_DK",
+    url: "https://mikkelpetersen.dk/",
+    site_name: "Mikkel Petersen",
+  },
+};
